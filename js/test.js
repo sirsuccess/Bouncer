1021e709b05e8f7c5503eb60a5694398bf0d758d
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-10-12 16:33:11 +0100

    make index page responsive

---
+++ js/test.js
@@ -0,0 +1,107 @@
+students = 
+[
+    {
+      name: 'Hendrick',
+      dob: '1853-07-18T00:00:00.000Z',
+      regNo: '041',
+    },
+    {
+      name: 'Albert',
+      dob: '1879-03-14T00:00:00.000Z',
+      regNo: '033',
+    },
+    {
+      name: 'Marie',
+      dob: '1867-11-07T00:00:00.000Z',
+      regNo: '024',
+    },
+    {
+      name: 'Neils',
+      dob: '1885-10-07T00:00:00.000Z',
+      regNo: '02',
+    },
+    {
+      name: 'Max',
+      dob: '1858-04-23T00:00:00.000Z',
+      regNo: '014',
+    },
+    {
+      name: 'Erwin',
+      dob: '1887-08-12T00:00:00.000Z',
+      regNo: '09',
+    },
+    {
+      name: 'Auguste',
+      dob: '1884-01-28T00:00:00.000Z',
+      regNo: '08',
+    },
+    {
+      name: 'Karl',
+      dob: '1901-12-05T00:00:00.000Z',
+      regNo: '120',
+    },
+    {
+      name: 'Louis', //
+      dob: '1892-08-15T00:00:00.000Z',
+      regNo: '022',
+    },
+    {
+      name: 'Arthur',
+      dob: '1892-09-10T00:00:00.000Z',
+      regNo: '321',
+    },
+    {
+      name: 'Paul',
+      dob: '1902-08-08T00:00:00.000Z',
+      regNo: '055',
+    },
+    {
+      name: 'William',
+      dob: '1890-03-31T00:00:00.000Z',
+      regNo: '013',
+    },
+    {
+      name: 'Owen',
+      dob: '1879-04-26T00:00:00.000Z',
+      regNo: '052',
+    },
+    {
+      name: 'Martin',
+      dob: '1871-02-15T00:00:00.000Z',
+      regNo: '063',
+    },
+    {
+      name: 'Guye',
+      dob: '1866-10-15T00:00:00.000Z',
+      regNo: '084',
+    },
+    {
+      name: 'Charles',
+      dob: '1868-02-14T00:00:00.000Z',
+      regNo: '091',
+    },
+  ];
+
+// students.map(student=>{
+//     newArr = [...student,]
+//     students=student
+// })
+
+console.log(students)
+c = {"amani": 2}
+b = Object.assign({},  c, student, )
+console.log(b)
+
+
+const a = [{ a: 1 }, { a: 2 }, { a: 3 }];
+
+const newA = a.map(value => {
+  value.b = value.a;
+  return value;
+  
+  const newValue = Object.assign({}, value, { b: value.a });
+  return newValue;
+});
+
+console.log(a);
+console.log(newA);
